d2f99a3a1637bbd55fc2ea17f68fe21f4dbe60b5
Tony Xie <tonyxie@Tonys-MacBook-Pro.local>
2018-12-20 16:26:59 -0800

    added routes for created new threads and comments

--- Migrations/UltimateBoardsContextModelSnapshot.cs
+++ Migrations/UltimateBoardsContextModelSnapshot.cs
@@ -17,6 +17,68 @@ namespace UltimateBoards.Migrations
                 .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
+            modelBuilder.Entity("UltimateBoards.Models.Category", b =>
+                {
+                    b.Property<int>("CategoryId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("CategoryId");
+
+                    b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("UltimateBoards.Models.Thread", b =>
+                {
+                    b.Property<int>("ThreadId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("CategoryId");
+
+                    b.Property<string>("Content")
+                        .IsRequired();
+
+                    b.Property<DateTime>("CreatedAt");
+
+                    b.Property<DateTime>("UpdatedAt");
+
+                    b.Property<int>("UserId");
+
+                    b.HasKey("ThreadId");
+
+                    b.HasIndex("CategoryId");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("Threads");
+                });
+
+            modelBuilder.Entity("UltimateBoards.Models.ThreadComment", b =>
+                {
+                    b.Property<int>("ThreadCommentId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Comment")
+                        .IsRequired();
+
+                    b.Property<DateTime>("CreatedAt");
+
+                    b.Property<int>("ThreadId");
+
+                    b.Property<DateTime>("UpdatedAt");
+
+                    b.Property<int>("UserId");
+
+                    b.HasKey("ThreadCommentId");
+
+                    b.HasIndex("ThreadId");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("ThreadComments");
+                });
+
             modelBuilder.Entity("UltimateBoards.Models.User", b =>
                 {
                     b.Property<int>("UserId")
@@ -41,6 +103,32 @@ namespace UltimateBoards.Migrations
                     b.HasKey("UserId");
 
                     b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("UltimateBoards.Models.Thread", b =>
+                {
+                    b.HasOne("UltimateBoards.Models.Category", "Category")
+                        .WithMany()
+                        .HasForeignKey("CategoryId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("UltimateBoards.Models.User", "User")
+                        .WithMany("Threads")
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("UltimateBoards.Models.ThreadComment", b =>
+                {
+                    b.HasOne("UltimateBoards.Models.Thread", "Thread")
+                        .WithMany("Comments")
+                        .HasForeignKey("ThreadId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("UltimateBoards.Models.User", "User")
+                        .WithMany("Comments")
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade);
                 });
 #pragma warning restore 612, 618
         }
